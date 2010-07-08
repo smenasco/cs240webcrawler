79927a00fc9e595796260f8db578022f3e6c05d7
smenasco <smenasco@6a29c3b7-13f5-95d4-5512-c4ab283dc354>
2010-07-08 02:03:48 +0000

    Created new branch to work on

---
+++ branches/webcrawler/src/main.cpp
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <iostream>
+#include "CS240Exception.h"
+using namespace std;
+
+int main(int argc, char * argv[]){
+	printOutput();
+	return 0;
+}
+
+void printOutput(){
+	
+	try{
+		cout << "Hello Make\n";
+	} catch (CS240Exception &e) {
+		cout << "Exception Occurred:" << e.GetMessage() <<endl;
+	}
+}
