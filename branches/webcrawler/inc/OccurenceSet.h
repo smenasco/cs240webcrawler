66724eb86c855cff2b77a3938aad690515fb1927
smenasco <smenasco@6a29c3b7-13f5-95d4-5512-c4ab283dc354>
2010-07-10 22:08:25 +0000

    Added OccurenceSet.cpp and header

---
+++ branches/webcrawler/inc/OccurenceSet.h
@@ -0,0 +1,13 @@
+#ifndef OCCURENCESET_H
+#define OCCURENCESET_H
+
+/*
+ *  OccurenceSet.h
+ *  project1
+ *
+ *  Created by Sam on 7/10/10.
+ *  Copyright 2010 Samuel Menasco. All rights reserved.
+ *
+ */
+
+#endif
---
+++ branches/webcrawler/src/OccurenceSet.cpp
@@ -0,0 +1,11 @@
+/*
+ *  OccurenceSet.cpp
+ *  project1
+ *
+ *  Created by Sam on 7/10/10.
+ *  Copyright 2010 Samuel Menasco. All rights reserved.
+ *
+ */
+
+#include "OccurenceSet.h"
+
