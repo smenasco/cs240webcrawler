5fca9e58272db685da2d2a735a019ee6b3f57f7d
smenasco <smenasco@6a29c3b7-13f5-95d4-5512-c4ab283dc354>
2010-07-11 01:06:31 +0000

    UPDATE: implements PageIndex using BST and Pages using PageNode class
    TODO:   STILL NEEDS TO HAVE URL CHANGED TO URL TYPE AND ADD PARAM TO CONSTUCTOR THAT WILL INITIALIZE THE DESCRIPTION OF THE PAGE

--- branches/webcrawler/inc/PageIndex.h
+++ branches/webcrawler/inc/PageIndex.h
@@ -8,5 +8,137 @@
  *  Copyright 2010 Samuel Menasco. All rights reserved.
  *
  */
+//NOTE: STILL NEEDS TO HAVE URL CHANGED TO URL TYPE AND ADD PARAM TO CONSTUCTOR
+//		THAT WILL INITIALIZE THE DESCRIPTION OF THE PAGE
+
+//A PageIndex of visited & indexed pages
+//store index of visited pages
+/*
+ URL url
+ if url is not inside pageindex 
+	if url is not in pagequeue 
+		add to pagequeue
+	else 
+		already queued to download
+ else 
+	its was already indexed
+ */
+#include <string>
+
+//!  PageNode implements a binary search tree node for a HTML Page
+class PageNode {
+	friend class PageIndex;   //!< PageIndex can access private members of PageNode
+	
+public:
+	
+	//!  Constructor
+	PageNode(const std::string & v) : url(v), left(NULL), right(NULL) {
+	}
+	
+	//!  Read-only public methods for use by clients of the PageIndex class
+	const std::string & GetValue() const {
+		return url;
+	}
+	
+	const PageNode * GetLeft() const {
+		return left;
+	}
+	
+	PageNode * GetLeft()	{
+		return left;
+	}
+	
+	const PageNode * GetRight() const {
+		return right;
+	}
+	
+	PageNode * GetRight() {
+		return right;
+	}
+	
+private:
+	std::string url;			//!< value stored in the node
+	std::string description;	//!< string containing the description of the Page
+	PageNode * left;			//!< pointer to the node's left child
+	PageNode * right;			//!< pointer to the node's right child
+};
+
+
+//!  PageIndex implements a binary search tree
+class PageIndex {
+private:
+	int count;
+	PageNode * root;
+	
+	void InInsert(PageNode * n);
+	PageNode * InsertAgain(PageNode * n, const std::string & v);
+	PageNode * FindAgain(PageNode * n,const std::string & v) const;
+	void Free(PageNode * n);
+public:
+	
+	//!  No-arg constructor.  Initializes an empty PageIndex
+	PageIndex();
+	
+	
+	//!  Copy constructor.  Makes a complete copy of its argument
+	PageIndex(const PageIndex & other);
+	
+	
+	//!  Destructor
+	~PageIndex();
+	
+	
+	//!  Assignment operator.  Makes a complete copy of its argument
+	//!  @return Reference to oneself
+	PageIndex& operator =(const PageIndex & other);
+	
+	
+	//!  @return a pointer to the root node of the tree, or NULL if the tree is empty.
+	//!  @note This is useful for PageIndex clients that need to traverse the tree.)
+	PageNode * GetRoot();
+	
+	
+	//!  @return true if the PageIndex is empty, or false if the PageIndex is not empty
+	bool IsEmpty() const;
+	
+	
+	//!  Removes all values from the PageIndex
+	void Clear();
+	
+	
+	//!  @return the number of values in the PageIndex
+	int GetSize() const;
+	
+	
+	//!  Inserts value v into the PageIndex
+	//!  
+	//!  @param v The new value being inserted
+	//!
+	//!  @return a pointer to the newly inserted node, or NULL if v was already
+	//!          in the tree (i.e., NULL is used to indicate a duplicate insertion)
+	PageNode * Insert(const std::string & v);
+	
+	
+	//!  Searches the tree for value v
+	//!  
+	//!  @param v The new value being searched for
+	//!
+	//!  @return a pointer to the node containing v, or NULL if v is not in the tree
+	PageNode * Find(const std::string & v) const;
+	
+	
+	//! @NOTE: YOU ARE NOT REQUIRED TO IMPLEMENT THE Remove METHOD BELOW
+	//!        (BUT YOU CAN IF YOU WANT TO)
+	//!
+	//!  Removes value v from the tree
+	//!  
+	//!  @param v The value being removed from the tree
+	//!
+	//!  @return true if v was removed from the tree, or false if v was not in the tree
+	//bool Remove(const std::string & v);
+	
+	
+};
+
 
 #endif
--- branches/webcrawler/src/PageIndex.cpp
+++ branches/webcrawler/src/PageIndex.cpp
@@ -9,3 +9,166 @@
 
 #include "PageIndex.h"
 
+#include <string>
+
+
+
+//! Initialize a new PageIndex from ref & other
+void PageIndex::InInsert(PageNode * n){
+	if (n == NULL) {
+		return;
+	} 
+	Insert(n->url);
+	if(n->left != NULL)
+		InInsert(n->left);
+	if(n->right != NULL)
+		InInsert(n->right);
+}
+//! call delete on all the elements in the PageIndex
+void PageIndex::Free(PageNode * n) {
+	if (n == NULL) 
+		return;
+	if(n->left != NULL)
+		Free(n->left);
+	if(n->right != NULL)
+		Free(n->right);
+	delete n;
+	count = 0;
+}
+PageIndex::PageIndex() : count(0), root(0){
+	return;
+}
+
+
+//!  Copy constructor.  Makes a complete copy of its argument
+PageIndex::PageIndex(const PageIndex & other){
+	root = 0;
+	count = 0;
+	PageNode * n = other.root;
+	InInsert(n);
+}
+
+//!  Destructor
+PageIndex::~PageIndex() {
+	Free(root);
+}
+
+
+//!  Assignment operator.  Makes a complete copy of its argument
+//!  @return Reference to oneself
+PageIndex& PageIndex::operator =(const PageIndex & other){
+	if (this != &other) {
+		PageNode * n = other.root;
+		Free(root);
+		root=NULL;
+		count=0;
+		InInsert(n);
+	}
+	return *this;
+}
+
+
+//!  @return a pointer to the root node of the tree, or NULL if the tree is empty.
+//!  @note This is useful for PageIndex clients that need to traverse the tree.)
+PageNode * PageIndex::GetRoot() {
+	if (IsEmpty())
+		return NULL;
+	return root;
+}
+
+
+//!  @return true if the PageIndex is empty, or false if the PageIndex is not empty
+bool PageIndex::IsEmpty() const {
+	return (count ==0);
+}
+
+
+//!  Removes all urls from the PageIndex
+void PageIndex::Clear() {
+	Free(root);
+	root = 0;
+	count =0;
+}
+
+
+//!  @return the number of urls in the PageIndex
+int PageIndex::GetSize() const {
+	return count;
+}
+
+
+//!  Inserts url v into the PageIndex
+//!  
+//!  @param v The new url being inserted
+//!
+//!  @return a pointer to the newly inserted node, or NULL if v was already
+//!          in the tree (i.e., NULL is used to indicate a duplicate insertion)
+
+
+PageNode * PageIndex::InsertAgain(PageNode * n, const std::string & v) {
+	if (n == NULL) {
+		n = new PageNode(v);
+		return n;
+	}
+	
+	if (v == n->url)
+		return NULL;
+	if (v > n->url){
+		if (n->right == NULL) {
+			n->right = new PageNode(v);
+			count++;
+			return n->right;
+		} else
+			return InsertAgain(n->right,v);
+		
+	} else {
+		if (n->left == NULL){
+			n->left = new PageNode(v);
+			count++;
+			return n->left;
+		} else
+			return InsertAgain(n->left,v);
+	}
+}
+
+PageNode * PageIndex::Insert(const std::string & v) {
+	if (root == NULL) {
+		root = new PageNode(v);
+		count++;
+		return root;
+	}
+	return InsertAgain(root,v);
+}
+
+
+//!  Searches the tree for url v
+//!  
+//!  @param v The new url being searched for
+//!
+//!  @return a pointer to the node containing v, or NULL if v is not in the tree
+PageNode * PageIndex::FindAgain(PageNode * n,const std::string & v) const {
+	if (n == NULL)
+		return NULL;
+	if (n->url == v)
+		return n;
+	if (v > n->url) {
+		return FindAgain(n->right,v);
+	} else
+		return FindAgain(n->left,v);
+}
+
+PageNode * PageIndex::Find(const std::string & v) const {
+	return FindAgain(root,v);
+}
+
+
+//! @NOTE: YOU ARE NOT REQUIRED TO IMPLEMENT THE Remove METHOD BELOW
+//!        (BUT YOU CAN IF YOU WANT TO)
+//!
+//!  Removes url v from the tree
+//!  
+//!  @param v The url being removed from the tree
+//!
+//!  @return true if v was removed from the tree, or false if v was not in the tree
+/*bool PageIndex::Remove(const std::string & v) {
+ }*/
