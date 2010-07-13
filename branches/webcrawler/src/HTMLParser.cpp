1b74d5e297ee6b0abaf30d080e88b29e7b519ec3
smenasco <smenasco@6a29c3b7-13f5-95d4-5512-c4ab283dc354>
2010-07-13 02:11:22 +0000

    UPDATE: 	-HTMLParser.cpp and header: I roughed together the class without any thought of what it will actually do... yet. 	-XMLGenerator: I included some files I wasnt ready to put to use so I un-included them.

--- branches/webcrawler/src/HTMLParser.cpp
+++ branches/webcrawler/src/HTMLParser.cpp
@@ -10,3 +10,39 @@
 #include "HTMLParser.h"
 #include "HTMLTokenizer.h"
 
+//! Delete HTMLParser from memory
+void HTMLParser::Free(){
+	
+}
+
+//! Called from constructor, copy constructor, and assignment operator
+void HTMLParser::Init(const HTMLParser & other){
+	
+}
+
+
+
+
+//!  No-arg constructor.  Initializes an empty HTMLParser
+HTMLParser::HTMLParser(){
+	
+}
+
+
+//!  Copy constructor.  Makes a complete copy of its argument
+HTMLParser::HTMLParser(const HTMLParser & other){
+	
+}
+
+
+//!  Destructor
+HTMLParser::~HTMLParser(){
+	
+}
+
+
+//! Assignment operator.  Makes a complete copy of its argument
+//! @return A reference to oneself
+HTMLParser& HTMLParser::operator =(const HTMLParser & other){
+	
+}
