eae5769a95e29bdde63e2cc69087b0f8d4604f01
smenasco <smenasco@6a29c3b7-13f5-95d4-5512-c4ab283dc354>
2010-07-18 03:26:22 +0000

    UPDATE: 	Main didn't handle itself very well if it wasn't handed valid args.  Fixed.

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char * argv[]){
 	//assert(argc ==4);
 	if (argc != 4) {
 		std::cout << "USAGE: webcrawler <start-url> <output-file> <stopwords-file>\n";
+		return -1;
 	}
 	WebCrawler * wc = new WebCrawler(argv[1],argv[2],argv[3]);
 	// to test use file:///Users/smenasco/Desktop/cs240/project1/webcrawler/file/student/index.html
